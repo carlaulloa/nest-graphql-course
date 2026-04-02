db936ef8a7aaa75b0034feaf00e9cf81920bf4a2
carlaulloa <carla.ulloa.lp@gmail.com>
2026-04-02 12:18:27 -0500

    feat(users): add authentication guards and role-based filtering

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,21 +2,30 @@ import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
   async findAll(
-    @Args() validRoles: ValidRolesArgs
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.Admin]) user: User
   ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
   @Query(() => User, { name: 'user' })
-  async findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
-    throw new Error('Method not implemented.');
+  async findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.Admin]) user: User
+  ): Promise<User> {
+    return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
