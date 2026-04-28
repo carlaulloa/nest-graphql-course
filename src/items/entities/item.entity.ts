c3cbd23a482a32fb876be211fdaea827dfebfe7a
carlaulloa <carla.ulloa.lp@gmail.com>
2026-04-27 22:11:20 -0500

    feat: add relation items to user

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,5 +1,6 @@
 import { ObjectType, Field, Int, ID, Float } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'items' })
 @ObjectType()
@@ -12,12 +13,13 @@ export class Item {
   @Field(() => String)
   name: string;
 
-  @Column({ type: 'float' })
-  @Field(() => Float)
-  quantity: number;
-
   @Column({ nullable: true })
   @Field(() => String, { nullable: true })
   quantityUnits: string;
+
+  @ManyToOne(() => User, (user) => user.items)
+  @Field(() => User)
+  user: User
+  
 
 }
