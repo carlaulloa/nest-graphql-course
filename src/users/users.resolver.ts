934eeb8ac804f7c7396e3e699f42d6b6d9c84564
carlaulloa <carla.ulloa.lp@gmail.com>
2026-04-30 22:32:08 -0500

    feat: resolve field

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, ID, ResolveField, Parent } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
@@ -7,11 +7,14 @@ import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UpdateUserInput } from './dto/UpdateUserInput';
+import { ItemsService } from 'src/items/items.service';
 
 @Resolver(() => User)
 @UseGuards(JwtAuthGuard)
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly itemsService: ItemsService) {}
 
   @Query(() => [User], { name: 'users' })
   async findAll(
@@ -42,6 +45,13 @@ export class UsersResolver {
     @Args('updateUserInput') updateUserInput: UpdateUserInput,
     @CurrentUser([ValidRoles.Admin]) user: User
   ): Promise<User> {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
+    return this.usersService.update(updateUserInput.id, updateUserInput, user);
+  }
+
+  @ResolveField(() => Int, { name: 'itemCount' })
+  async itemCount(
+    @Parent() user: User
+  ): Promise<number> {
+    return this.itemsService.itemCountByUser(user);
   }
 }
